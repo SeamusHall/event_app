41082f5d96b79a2425a02e919e68ab61cc3aec6d
Adam Woodworth <adamwoodworth94@siu.edu>
2017-04-18 21:32:40 -0500

    add production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "eclipse.housing.siu.edu" }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address  => "cstmta4.siu.edu",
+    :port  => 25,
+    :domain  => "siu.edu",
+    # added due to self signed certificates
+    :enable_starttls_auto => true,
+    :openssl_verify_mode  => 'none'
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
