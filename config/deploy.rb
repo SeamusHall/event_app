7e2fdb4ff18ccd2694c0d43086379b6ca18a61b3
jmiller70 <jmiller@housing.siu.edu>
2017-05-04 13:48:38 -0500

    verbatim

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,45 @@
-
+# Change these
 server '131.230.116.221', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:UniversityHousing/events.git'
 set :application,     'events'
 set :user,            'deployer'
 set :puma_threads,    [8, 32]
-set :puma_workers,    2      # Server can handle it
+set :puma_workers,    2
 
+# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/var/www/events"
-set :puma_bind,       "unix:///var/www/events/shared/tmp/sockets/events-puma.sock"
-set :puma_state,      "/var/www/events/shared/tmp/pids/puma.state"
-set :puma_pid,        "/var/www/events/shared/tmp/pids/puma.pid"
-set :puma_access_log, "/var/www/events/log/puma.error.log"
-set :puma_error_log,  "/var/www/events/log/puma.access.log"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
-set :puma_worker_timeout, 4 * 3600      # 4 hours in seconds
+set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
-#set :repository, "deployer@131.230.116.221:/var/www/events"
-#set :scm,           :git
-set :branch,        :master
-set :format,        :pretty
-set :log_level,     :debug
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir /var/www/events/shared/ -p"
-      execute "mkdir /var/www/events/shared/tmp/sockets -p"
-      execute "mkdir /var/www/events/shared/tmp/pids -p"
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
@@ -79,3 +78,7 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
