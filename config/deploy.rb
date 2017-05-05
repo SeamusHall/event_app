0e0a8db578c05b7c552ef212abe4ed5cd9ed9dcf
phantomfreeJr <vdavis@siu.edu>
2017-05-05 00:27:29 -0500

    really close

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,17 +10,19 @@ set :user,            'deployer'
 set :puma_threads,    [8, 32]
 set :puma_workers,    2      # Server can handle it
 
+set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
-set :deploy_to,       "/var/www/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_dir}/sockets/puma.sock"
-set :puma_state,      "#{shared_dir}/pids/puma.state"
-set :puma_pid,        "#{shared_dir}/pids/puma.pid"
-set :puma_access_log, "#{shared_dir}/log/puma.stderr.log"
-set :puma_error_log,  "#{shared_dir}/log/puma.stdout.log"
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
-set :puma_worker_timeout, 4 * 3600      # 4 hours in seconds
+set :puma_worker_timeout, 4 * 3600 # 4 hours in seconds
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
@@ -33,9 +35,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_dir}/sockets -p"
-      execute "mkdir #{shared_dir}/pids -p"
-      execute "mkdir #{shared_dir}/log -p"
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
@@ -54,54 +55,9 @@ namespace :deploy do
     end
   end
 
-  desc 'DB Migrations'
-  task :db_migrations do
-    on roles(:db) do
-      # Skip migration if files in db/migrate were not modified
-      set :conditionally_migrate, true
-      with path: "/var/www/#{fetch(:application)}" do
-        with rails_env: :production do
-          rake "db:migrate"
-        end
-      end
-    end
-  end
-
-  desc 'Pull From Git'
-  task :git_pull do
-    on roles(:app) do
-      with path: "/var/www/#{fetch(:application)}" do
-        execute 'git pull'
-      end
-    end
-  end
-
-  desc 'Precompiling assets'
-  task :precompile_assets do
-    on roles(:app) do
-      with path: "/var/www/#{fetch(:application)}" do
-        with rails_env: :production do
-          rake "assets:precompile"
-        end
-      end
-    end
-  end
-
-  desc 'Install new gems'
-  task :bundle_install do
-    on roles(:app) do
-      with path: "/var/www/#{fetch(:application)}" do
-        with rails_env: :production do
-          execute 'bundle install'
-        end
-      end
-    end
-  end
-
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
   end
