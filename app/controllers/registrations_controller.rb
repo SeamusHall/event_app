aa0fce720ab0ecfb26e4ad0686b907935add8e37
phantomfreeJr <vdavis@siu.edu>
2017-05-24 15:33:15 -0500

    fix sign_up screen confusion

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def check_captcha
-    unless verify_recaptcha
+    if verify_recaptcha
       self.resource = resource_class.new sign_up_params
       respond_with_navigational(resource) { render :new }
     end
