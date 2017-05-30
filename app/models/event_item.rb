da7af241ea0658a2cd582d4508a630b8b85f8363
phantomfreeJr <vdavis@siu.edu>
2017-05-30 13:28:36 -0500

    update event order safety blanket

--- app/models/event_item.rb
+++ app/models/event_item.rb
@@ -5,7 +5,8 @@ class EventItem < ApplicationRecord
   validates_presence_of :description, :price, :max_event, :max_order
   validates :price, numericality: { greater_than: 0 }
   validates :tax, numericality: { less_than: 0.99 }
-  validates :max_event, :max_order, numericality: { only_integer: true , greater_than: 0 }
+  validates :max_order, numericality: { only_integer: true , greater_than: 0 }
+  validates :max_event, numericality: { only_integer: true , greater_than: -1 }
   #validate :valid_min_freq
 
   private
