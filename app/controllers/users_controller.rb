57ee7e2239c0bbe0d27e9a15cda7bf5824604864
phantomfreeJr <vdavis@siu.edu>
2017-08-08 20:03:49 -0500

    update user soft_delete,true_delete, & restore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,16 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-
-  # Important Don't Permit Role Ids
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :phone, :address, :city, :state, :country, :postal_code)
+  def destroy
+    # Completely delete user and anything related to that user
+    orders = Order.where(user_id: @user.id) + OrderProduct.where(user_id: @user.id)
+    orders.each { |o| o.really_destroy! }
+    redirect_to root_path, notice: (@user.really_destroy!) ? "User was successfully removed" : "#{@user.full_name} was not successfully removed"
   end
+
+  private
+    # Important Don't Permit Role Ids here
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :phone, :address, :city, :state, :country, :postal_code)
+    end
 end
